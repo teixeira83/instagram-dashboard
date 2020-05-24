15406d1c94afa5f01977e73a9f859df3a1d46205
Rafael <rafael.amtx@gmail.com>
2020-05-24 13:01:16 -0300

    create view homepage

---
+++ src/index.js
@@ -0,0 +1,46 @@
+const { app, BrowserWindow } = require('electron')
+
+require('electron-reload')(__dirname,{
+    electron: require(`${__dirname}/../node_modules/electron`)
+});
+
+function createWindow () {
+  // Cria uma janela de navegação.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('./public/index.html')
+
+  // Open the DevTools.
+  win.webContents.openDevTools()
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Algumas APIs podem ser usadas somente depois que este evento ocorre.
+app.whenReady().then(createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // No macOS é comum para aplicativos e sua barra de menu 
+  // permaneçam ativo até que o usuário explicitamente encerre com Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
